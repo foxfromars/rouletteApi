24838dd35dc5e9e68be2f9ae6463c0533caca565
Luis Felipe <luisfeliperaposobatista@gmail.com>
2022-09-22 13:35:18 -0300

    124

--- utils/RouletteData/selectFrame.js
+++ utils/RouletteData/selectFrame.js
@@ -7,10 +7,18 @@ async function selectFrame(browser , page){
     const iframeHandle = await page.$("iframe");
     const contentFrame = await iframeHandle.contentFrame();
     // await contentFrame.waitForSelector("li.sidebar-buttons__item:nth-of-type(5)")
-    await contentFrame.click("li.sidebar-buttons__item:nth-of-type(5)");
+    await contentFrame.evaluate(()=>{
+      Array.from(
+        document.querySelectorAll("li.sidebar-buttons__item")
+      ).map((e,index)=>{
+        if(index===4){
+          e.click
+        } 
+      })
+    });
     // await contentFrame.waitForSelector(
     //   ".roulette-history-extended__contentxfUxn2lfztU7EVIL05oM > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)"
-    // );
+    // );"li.sidebar-buttons__item"
     return contentFrame
   }
 
