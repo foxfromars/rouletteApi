7cef4bf48b35b3d9c14b56d064508a007ff401c2
Luis Felipe <luisfeliperaposobatista@gmail.com>
2022-09-22 12:40:48 -0300

    await selector adicionado

--- utils/RouletteData/selectFrame.js
+++ utils/RouletteData/selectFrame.js
@@ -4,6 +4,7 @@ async function selectFrame(browser , page){
     await page.waitForSelector("iframe");
     const iframeHandle = await page.$("iframe");
     const contentFrame = await iframeHandle.contentFrame();
+    await contentFrame.waitForSelector("li.sidebar-buttons__item:nth-of-type(5)")
     await contentFrame.click("li.sidebar-buttons__item:nth-of-type(5)");
     // await contentFrame.waitForSelector(
     //   ".roulette-history-extended__contentxfUxn2lfztU7EVIL05oM > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)"
